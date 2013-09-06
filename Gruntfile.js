fcf75973185f903c352665afa247052cef9ab09a
Cory Gibbons <hello@corygibbons.com>
2013-09-05 22:53:57 -0700

    Add Grunt to aid in  development workflow with minification, etc

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+
+        watch: {
+            compass: {
+                files: ['scss/**/*.{scss,sass}'],
+                tasks: ['compass']
+            }
+        },
+
+
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        }
+
+    });
+
+
+    grunt.registerTask('default', ['watch']);
+
+};
---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "/"
+sass_dir = "scss"
+
+output_style = :compressed
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
