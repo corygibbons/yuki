d3785a772ea68be1f89d96cf57fe4603f9be037b
Cory Gibbons <hello@corygibbons.com>
2016-06-25 04:02:52 -0700

    Setup new build system for Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('sass', function() {
-    return gulp.src('sass/**/*.sass')
-    .pipe(sass({style: 'compressed'}))
+
+gulp.task('default', ['stylus'], function() {
+    gulp.watch('src/stylus/**/*.styl', ['stylus']);
+});
+
+
+gulp.task('stylus', function() {
+    console.log('piped to stylus');
+    gulp.src('src/stylus/*.styl')
+    .pipe(stylus({
+        'include css': true,
+        'compress': true
+    }))
     .pipe(autoprefixer('last 15 version'))
     .on('error', function(err) { console.log(err.message); })
     .pipe(gulp.dest('.'));
-});
-
-gulp.task('default', function() {
-    gulp.watch('sass/**/*.sass', function() {
-        gulp.run('sass');
-    });
 });
